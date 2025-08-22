adf510b94d02c462db1c092e0b37f4a9d8e8c310
RB2678 <74243718+RB2678@users.noreply.github.com>
2025-08-22 13:46:39 +0300

    Update tg-bot.py

--- tg-bot.py
+++ tg-bot.py
@@ -26,7 +26,7 @@ app = Flask(__name__)
 MAX_LEN = 4096
 
 def escape_markdown(text: str) -> str:
-    escape_chars = r'\_*[]()~`>+-=|{}.!'
+    escape_chars = r'\_[]()~`>+-=|{}.!'
     return re.sub(f'([{re.escape(escape_chars)}])', r'\\\1', text)
 
 # # format_with_html
